159dc851ce9248282b54998c1ee2e0124cb076b4
stasgurevich <stas.gurevich77@gmail.com>
2019-12-14 18:18:57 +0300

    param fix

--- add_pkg.sh
+++ add_pkg.sh
@@ -1,7 +1,7 @@
 cd /home/pi/kinetic_ws
-rosinstall_generator %1 --rosdistro kinetic --deps --wet-only --tar > add.rosinstall
+rosinstall_generator $1 --rosdistro kinetic --deps --wet-only --tar > add.rosinstall
 wstool merge -t src add.rosinstall
 wstool update -t src -j4
 rm src/gazebo_ros_pkgs -rf
 rm src/ackermann_steering_controller -rf
-sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/kinetic -j1 -DCATKIN_ENABLE_TESTING=0 --pkg %1
+sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/kinetic -j1 -DCATKIN_ENABLE_TESTING=0 --pkg $1
